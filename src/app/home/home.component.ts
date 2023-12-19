4388141db434bd2b229213dc9ada024446eb3951
Drew Layton <dlayton66@gmail.com>
2023-12-19 11:32:25 -0600

    Comment out existing home code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from "@angular/core";
-import fetchFromSpotify, { request } from "../../services/api";
+// import fetchFromSpotify, { request } from "../../services/api";
 
-const AUTH_ENDPOINT =
-  "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
-const TOKEN_KEY = "whos-who-access-token";
+// const AUTH_ENDPOINT =
+//   "https://nuod0t2zoe.execute-api.us-east-2.amazonaws.com/FT-Classroom/spotify-auth-token";
+// const TOKEN_KEY = "whos-who-access-token";
 
 @Component({
   selector: "app-home",
@@ -13,40 +13,40 @@ const TOKEN_KEY = "whos-who-access-token";
 export class HomeComponent implements OnInit {
   constructor() {}
 
-  genres: String[] = ["House", "Alternative", "J-Rock", "R&B"];
-  selectedGenre: String = "";
-  authLoading: boolean = false;
-  configLoading: boolean = false;
-  token: String = "";
+  // genres: String[] = ["House", "Alternative", "J-Rock", "R&B"];
+  // selectedGenre: String = "";
+  // authLoading: boolean = false;
+  // configLoading: boolean = false;
+  // token: String = "";
 
   ngOnInit(): void {
-    this.authLoading = true;
-    const storedTokenString = localStorage.getItem(TOKEN_KEY);
-    if (storedTokenString) {
-      const storedToken = JSON.parse(storedTokenString);
-      if (storedToken.expiration > Date.now()) {
-        console.log("Token found in localstorage");
-        this.authLoading = false;
-        this.token = storedToken.value;
-        this.loadGenres(storedToken.value);
-        return;
-      }
-    }
-    console.log("Sending request to AWS endpoint");
-    request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
-      const newToken = {
-        value: access_token,
-        expiration: Date.now() + (expires_in - 20) * 1000,
-      };
-      localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
-      this.authLoading = false;
-      this.token = newToken.value;
-      this.loadGenres(newToken.value);
-    });
-  }
+  //   this.authLoading = true;
+  //   const storedTokenString = localStorage.getItem(TOKEN_KEY);
+  //   if (storedTokenString) {
+  //     const storedToken = JSON.parse(storedTokenString);
+  //     if (storedToken.expiration > Date.now()) {
+  //       console.log("Token found in localstorage");
+  //       this.authLoading = false;
+  //       this.token = storedToken.value;
+  //       this.loadGenres(storedToken.value);
+  //       return;
+  //     }
+  //   }
+  //   console.log("Sending request to AWS endpoint");
+  //   request(AUTH_ENDPOINT).then(({ access_token, expires_in }) => {
+  //     const newToken = {
+  //       value: access_token,
+  //       expiration: Date.now() + (expires_in - 20) * 1000,
+  //     };
+  //     localStorage.setItem(TOKEN_KEY, JSON.stringify(newToken));
+  //     this.authLoading = false;
+  //     this.token = newToken.value;
+  //     this.loadGenres(newToken.value);
+  //   });
+  // }
 
-  loadGenres = async (t: any) => {
-    this.configLoading = true;
+  // loadGenres = async (t: any) => {
+  //   this.configLoading = true;
 
     // #################################################################################
     // DEPRECATED!!! Use only for example purposes
@@ -59,24 +59,24 @@ export class HomeComponent implements OnInit {
     // console.log(response);
     // #################################################################################
     
-    this.genres = [
-      "rock",
-      "rap",
-      "pop",
-      "country",
-      "hip-hop",
-      "jazz",
-      "alternative",
-      "j-pop",
-      "k-pop",
-      "emo"
-    ]
-    this.configLoading = false;
+    // this.genres = [
+    //   "rock",
+    //   "rap",
+    //   "pop",
+    //   "country",
+    //   "hip-hop",
+    //   "jazz",
+    //   "alternative",
+    //   "j-pop",
+    //   "k-pop",
+    //   "emo"
+    // ]
+    // this.configLoading = false;
   };
 
-  setGenre(selectedGenre: any) {
-    this.selectedGenre = selectedGenre;
-    console.log(this.selectedGenre);
-    console.log(TOKEN_KEY);
-  }
+  // setGenre(selectedGenre: any) {
+  //   this.selectedGenre = selectedGenre;
+  //   console.log(this.selectedGenre);
+  //   console.log(TOKEN_KEY);
+  // }
 }
