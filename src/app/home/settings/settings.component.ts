afe264fc3a0fb548f2caa9af64ec32b96f3027c3
Drew Layton <dlayton66@gmail.com>
2023-12-19 16:35:42 -0600

    feat: Tweak errors and show current genre by default

--- src/app/home/settings/settings.component.ts
+++ src/app/home/settings/settings.component.ts
@@ -1,54 +1,45 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { SettingsService } from 'src/app/settings.service';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { SettingsService } from "src/app/settings.service";
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+  selector: "app-settings",
+  templateUrl: "./settings.component.html",
+  styleUrls: ["./settings.component.css"],
 })
-
 export class SettingsComponent implements OnInit {
-  
-  @Output() toggleSettingsEvent = new EventEmitter<void>()
+  @Output() toggleSettingsEvent = new EventEmitter<void>();
 
-  genres: string[] = []
-  showingErrors: boolean = false
-  
+  genres: string[] = ["Rock", "Rap", "Pop", "Country", "Alternative"];
+  currentGenre: string = "";
+  showingErrors: boolean = false;
+
   settingsForm: FormGroup = new FormGroup({
-    selectedGenre: new FormControl<string>('', [Validators.required])
-  })
+    selectedGenre: new FormControl<string>("", [Validators.required]),
+  });
 
-  constructor(private settingsData: SettingsService) { }
+  constructor(private settingsData: SettingsService) {}
 
   ngOnInit(): void {
-    this.genres = [
-      "rock",
-      "rap",
-      "pop",
-      "country",
-      "alternative"
-    ]
+    this.settingsData.genre.subscribe((genre) => (this.currentGenre = genre));
+    this.settingsForm.setValue({ selectedGenre: this.currentGenre });
   }
 
   onSubmit() {
-    if(this.settingsForm.valid) {
+    if (this.settingsForm.valid) {
+      this.showingErrors = false;
       this.settingsData.updateGenre(
-        this.settingsForm.controls['selectedGenre'].value
-      )
-      this.passToggleSettings()
-  
-      console.log(this.settingsData.genre) // log observable
-      console.log(this.settingsForm.controls['selectedGenre'].errors)
+        this.settingsForm.controls["selectedGenre"].value
+      );
+      this.passToggleSettings();
     }
   }
 
   passToggleSettings() {
-    this.toggleSettingsEvent.emit()
+    this.toggleSettingsEvent.emit();
   }
 
   showErrors() {
-    this.showingErrors = true
+    this.showingErrors = true;
   }
-
 }
