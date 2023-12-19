013afc4efc122ecfd7555a3e663bacb160a1b6b1
Drew Layton <dlayton66@gmail.com>
2023-12-19 14:55:52 -0600

    Update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
+import { ReactiveFormsModule } from '@angular/forms'
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { SettingsComponent } from './home/settings/settings.component';
 
 const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
+  declarations: [AppComponent, HomeComponent, SettingsComponent],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
 })
